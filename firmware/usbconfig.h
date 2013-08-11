7fa828b2956a8ae3af639bcbadc8b228e7a35cc0
Michał Słomkowski <m.slomkowski@gmail.com>
2013-08-11 23:52:07 +0200

    Reenabled interrupt endpoint for compatibility with USB spec.

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -82,7 +82,7 @@
 
 /* --------------------------- Functional Range ---------------------------- */
 
-#define USB_CFG_HAVE_INTRIN_ENDPOINT    0
+#define USB_CFG_HAVE_INTRIN_ENDPOINT    1
 /* Define this to 1 if you want to compile a version with two endpoints: The
  * default control endpoint 0 and an interrupt-in endpoint (any other endpoint
  * number).
