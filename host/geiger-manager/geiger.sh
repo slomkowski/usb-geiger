41e7affe191f7d0a8f358e99620c579e6e3559e6
Michał Słomkowski <m.slomkowski@gmail.com>
2014-08-05 12:48:56 +0200

    Update Debian init script.

--- host/geiger-manager/geiger.sh
+++ host/geiger-manager/geiger.sh
@@ -1,31 +1,57 @@
 #!/bin/sh
+ 
 ### BEGIN INIT INFO
-# Provides:          geiger 
+# Provides:          geiger
+# Required-Start:    $remote_fs
+# Required-Stop:     $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: USB Geiger monitor daemon.
-# Description:       Script for starting USB Geiger manager at boot time.
-# You should place it in /etc/init.d/geiger and give it executable premissions: 755.
-# To start it at boot time, type: 
-# sudo update-rc.d geiger defaults
-#
+# Short-Description: USB Geiger counter daemon
 ### END INIT INFO
+ 
+# Change it to the directory where you want to put the Python scripts
+DIR=/root/usb-geiger/host/geiger-manager
 
-
-USER='pi'
-GEIGER_PATH=/home/pi/geiger-manager
+DAEMON=$DIR/main.py
+DAEMON_NAME=geiger
+ 
+DAEMON_OPTS="-m"
+ 
+DAEMON_USER=root
+ 
+PIDFILE=/var/run/$DAEMON_NAME.pid
+ 
+. /lib/lsb/init-functions
+ 
+do_start () {
+    log_daemon_msg "Starting system $DAEMON_NAME daemon"
+    start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --user $DAEMON_USER --chuid $DAEMON_USER --startas $DAEMON -- $DAEMON_OPTS
+    log_end_msg $?
+}
+do_stop () {
+    log_daemon_msg "Stopping system $DAEMON_NAME daemon"
+    start-stop-daemon --stop --pidfile $PIDFILE --retry 10
+    log_end_msg $?
+}
+ 
 case "$1" in
-    start)
-        su $USER -c "python $GEIGER_PATH/main.py -mb"
-        echo "Starting Geiger manager on $USER "
+ 
+    start|stop)
+        do_${1}
         ;;
-    stop)
-        pkill -u $USER -f "python $GEIGER_PATH/main.py -mb"
-        echo "Geiger manager stopped"
+ 
+    restart|reload|force-reload)
+        do_stop
+        do_start
+        ;;
+ 
+    status)
+        status_of_proc "$DAEMON_NAME" "$DAEMON" && exit 0 || exit $?
         ;;
     *)
-        echo "Usage: /etc/init.d/geiger {start|stop}"
+        echo "Usage: /etc/init.d/$DAEMON_NAME {start|stop|restart|status}"
         exit 1
         ;;
+ 
 esac
 exit 0
